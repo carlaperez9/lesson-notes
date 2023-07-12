e44ffcd64241a0cbed0fc613bb775128e08fcbd4
Carla Perez Irizarry <pr62001@HR546830MAC.local>
2023-07-12 10:59:52 -0400

    first-commit

---
+++ week1/d3/src/Main.java
@@ -0,0 +1,8 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // In the earliest part of the class, we covered file handling and the Scanner class.
+
+        }
+    }
